96e4cf61f6d03011e43a03dc1834e94c0fee0a39
Samuel <samuelalves681@gmail>
2022-07-19 00:09:58 -0300

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,28 @@
-import styles from "../styles/home.module.scss";
+/* eslint-disable @next/next/no-img-element */
+import styles from "./home.module.scss";
 import Head from "next/head";
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1 className={styles.title}>Hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏Hey, welcome</span>
+
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
     </>
   );
 }
