8b2c996c722dbcfc9a2a6c89c1e5d24cac5363ad
Samuel <samuelalves681@gmail>
2022-07-19 14:12:32 -0300

    use ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import styles from "./home.module.scss";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
@@ -46,7 +46,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1LN6yHJuVgZiqXfqVbNM5ome");
 
   const product = {
@@ -57,5 +57,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
